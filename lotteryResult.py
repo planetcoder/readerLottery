caf18b1cd8923e6d070d2652f9969dabba50e81b
kiven <kiven.mr@gmail.com>
2016-10-26 20:27:41 +0800

    Format code with pep8 and add timeout to requests

--- lotteryResult.py
+++ lotteryResult.py
@@ -3,42 +3,43 @@
 import sys
 import json
 import requests
-import hashlib
 
 
-def hashToNumber(txhash,total):
-  result = long(txhash, 16) % total 
-  return result 
+def hashToNumber(txhash, total):
+    result = long(txhash, 16) % total
+    return result
+
 
 def getBlocktxs(blockhash, number, total, startnum):
-  url = "https://blockexplorer.com/api/block/" + blockhash
-  params = dict()
-  
-  resp = requests.get(url=url, params=params)
-  data = json.loads(resp.text)
-  if "tx" in data:
-    if len(data["tx"]) >= number :
-      print ("%d Transactions for %d results." % (len(data["tx"]), number) ) 
-      for i in range(number):
-        txhash=data["tx"][i];
-        r = hashToNumber  (txhash, total) + startnum
-        print ( "result %d is %d" % (i, r) )
-    else:
-      print ("only %d Transactions for %d results." % (len(data["tx"]), number) ) 
-      
+    url = "https://blockexplorer.com/api/block/" + blockhash
+    params = dict()
 
-  else:
-    print "invalid block data"
+    resp = requests.get(url=url, params=params, timeout=5)
+    data = json.loads(resp.text)
+    if "tx" in data:
+        if len(data["tx"]) >= number:
+            print ("%d Transactions for %d results." % (len(data["tx"]), number))
+            for i in range(number):
+                txhash=data["tx"][i]
+                r = hashToNumber(txhash, total) + startnum
+                print ("result %d is %d" % (i, r))
+        else:
+            print ("only %d Transactions for %d results." % (len(data["tx"]), number))
+
+    else:
+        print "invalid block data"
+
 
 def main():
-  if len(sys.argv) == 5:
-    blockhash = sys.argv[1]
-    number = sys.argv[2]
-    total= sys.argv[3]
-    startnum = sys.argv[4]
-    getBlocktxs(blockhash, int(number), int(total), int(startnum))
-  else:
-    print "usage: ./lotteryResult.py blockhash number total startnum"
+    if len(sys.argv) == 5:
+        blockhash = sys.argv[1]
+        number = sys.argv[2]
+        total= sys.argv[3]
+        startnum = sys.argv[4]
+        getBlocktxs(blockhash, int(number), int(total), int(startnum))
+    else:
+        print "usage: ./lotteryResult.py blockhash number total startnum"
+
 
 if __name__ == '__main__':
-  main()
+    main()
